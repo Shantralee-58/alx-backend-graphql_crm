f9c8d3b685c35049be38e2cc1dc09469d349cccb
Idah Lindiwe Khumalo <86086701+Shantralee-58@users.noreply.github.com>
2025-08-17 00:13:21 +0200

    Set Up GraphQL Endpoint

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,7 +1,7 @@
 import graphene
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello world")
+    hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
 
