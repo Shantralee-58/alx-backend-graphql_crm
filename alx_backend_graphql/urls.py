936514e4957d881bdabe489489c025aab85cfbe5
Idah Lindiwe Khumalo <86086701+Shantralee-58@users.noreply.github.com>
2025-08-17 19:51:56 +0200

    task 0 fixed

--- alx_backend_graphql/urls.py
+++ alx_backend_graphql/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to ALX Backend GraphQL CRM! Visit <a href='/graphql'>/graphql</a> for the GraphQL interface.")
 
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
